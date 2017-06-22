b847e9e45d5bfccd9731c39b5a30d33cc8a76673
Andrew Walker <a.walker@leeds.ac.uk>
2017-06-22 14:33:28 +0100

    Read data from .castep not .csv

--- bm3_eos.py
+++ bm3_eos.py
@@ -288,6 +288,76 @@ def read_data_file(filename, data):
 
     return data, ts
 
+def parse_castep_file(filename, current_data=[]):
+    """Read a Castep output file with thermodynamic data and extract it
+
+       This appends thermodynamic data, in the form of a tuple, for
+       each temperature at which lattice dynamics was used to calculate
+       thermodynamic data. The tuple includes (in order) the cell
+       volume (A**3), static internal energy including the finite basis
+       set correctio (eV), the zero point energy (eV), the temperature
+       (K), vibrational energy (eV), vibriational component of the
+       Helmohotz free energy (eV), the entropy (J/mol/K) and heat
+       capacity (J/mol/K). These tuples are returned in a list
+       in the order found in the file. Multiple runs can be joined
+       together in one file and this is handled correctly. If multile
+       files need to be parsed the function can be called repetedly
+       with the output list passed in as the optional current_data
+       argument and new data will be appended to this list.
+    """
+
+    ts = []
+    fh = open(filename, 'r')
+    current_volume = None
+    in_thermo = False
+    skip_lines = 0
+    for line in fh:
+        if skip_lines > 0:
+            skip_lines = skip_lines - 1
+            continue
+        match = _vol_re.search(line)
+        if match:
+            # We need to keep track of the *current* volume
+            current_volume = float(match.group(1))
+            continue
+        match = _ufb_re.search(line)
+        if match:
+            # We need to keep track of the *current* internal energy
+            U = float(match.group(1))
+            continue
+        match = _zpe_re.search(line)
+        if match:
+            # A line with the zero point energy must start a
+            # thermo block. We need to skip three
+            # lines first though.
+            zpe = float(match.group(1))
+            in_thermo = True
+            skip_lines = 3
+            continue
+        if in_thermo:
+            # This is the bulk of the data in the table
+            match = _tmo_re.search(line)
+            if match:
+                T = float(match.group(1))
+                E = float(match.group(2))
+                F = float(match.group(3))
+                S = float(match.group(4))
+                Cv = float(match.group(5))
+                current_data.append((current_volume, U, zpe, T,
+                                                       E, F, S, Cv))
+                ts.append(T)
+                continue
+            else:
+                # Must be at the end of this thermo table
+                in_thermo = False
+                zpe = None
+                U = None
+                current_volume = None
+                continue
+
+    fh.close()
+    return current_data, ts
+
 
 def get_VF(data_table, T):
     """Given the data file from parse_castep_file return useful data at T
@@ -387,8 +457,9 @@ if __name__ == "__main__":
     data = []
     for file in args.datafiles:
         print("Reading data from: ", file)
-        data, ts = read_data_file(file, data)
+        #data, ts = read_data_file(file, data)
         # NB: we assume that ts is the same for each file!
+        idata, ts = parse_castep_file(file, data)
 
     # Fit EOS parameters at each T and store
     vs = []
